718e978446caf2e4424f22ce8c35ea886914e4ed
Iga <iga.koninska@gmail.com>
2017-11-26 11:08:38 +0100

    Zmiana Igi

---
+++ Ankieta/src/app/Controller/LoginController.java
@@ -0,0 +1,40 @@
+package app.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private AnchorPane AP;
+
+    @FXML
+    private TextField txt_log;
+
+    @FXML
+    private PasswordField pf_pass;
+
+    @FXML
+    private TextField tf_pas;
+
+    @FXML
+    private Button btn_log;
+
+    @FXML
+    private Button btn_show;
+
+    @FXML
+    void buttonLogin(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showPass(MouseEvent event) {
+
+    }
+
+}
